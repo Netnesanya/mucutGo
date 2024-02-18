e103046aebc31d8dfb2ff4b01cf4139e3b8f7df0
netnesanya <Netnesanya@mail.ru>
2024-02-18 10:05:15 +0100

    Init websocket

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 )
 
 func NewRouter() *mux.Router {
@@ -18,5 +21,40 @@ func NewRouter() *mux.Router {
 
 	router.HandleFunc("/pack-siq", CreateSiqHandler).Methods("GET")
 
+	router.HandleFunc("/ws/video-info", videoInfoWebSocket)
+
 	return router
+}
+
+func videoInfoWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// Here, you would read messages from the client, such as a batch of video titles
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+
+		// Process the message and send updates back to the client
+		// For demonstration, just echo the message back
+		err = conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Adjust the origin checking for production use
+	},
 }
